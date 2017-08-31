41c4c79e0b4d41605874e48ef8aa325086d573ab
xuanzhiding <xuanzhiding@omatec.cn>
2017-08-31 23:18:08 +0800

    2017年8月31日23:18

--- app/src/main/java/com/mc/app/hotel/common/facealignment/view/AboutFragment.java
+++ app/src/main/java/com/mc/app/hotel/common/facealignment/view/AboutFragment.java
@@ -6,16 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.caihua.cloud.common.util.DeviceIdUtil;
 import com.mc.app.hotel.R;
-import com.mc.app.hotel.common.facealignment.service.CheckUpdateService;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Unbinder;
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AboutFragment#newInstance} factory method to
@@ -23,9 +16,6 @@ import butterknife.Unbinder;
  */
 public class AboutFragment extends Fragment {
 
-    @BindView(R.id.deviceIDTv)
-    TextView deviceIDTv;
-    Unbinder unbinder;
 
     public static AboutFragment newInstance() {
         AboutFragment fragment = new AboutFragment();
@@ -36,19 +26,11 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about, container, false);
-        unbinder = ButterKnife.bind(this, view);
-        deviceIDTv.setText(getString(R.string.DEVICE_ID) + DeviceIdUtil.generateDeviceId(getContext()));
         return view;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        unbinder.unbind();
-    }
-
-    @OnClick(R.id.checkUpdateBtn)
-    public void onViewClicked() {
-        CheckUpdateService.checkUpdateImmediately(getContext());
     }
 }
